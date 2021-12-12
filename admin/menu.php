37420c9ff4adc92b4a19d9fa91411a59db69731a
mendesleu <leonardo.mendes10@hotmail.com>
2021-12-12 15:23:00 -0300

    separate menu

---
+++ admin/menu.php
@@ -0,0 +1,9 @@
+    <header id="top">
+        <nav>
+            <ul>
+                <li><a href="list-post.php">Postagens</a></li>
+                <li><a href="">User</a></li>
+                <li><a href="">Categoria</a></li>
+            </ul>
+        </nav>
+    </header>
